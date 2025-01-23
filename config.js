416b7e83963d74bc3d8a2c86583cd3e6433ed7f8
hajjihaju <hajjihaju2@gmail.com>
2025-01-23 23:30:12 +0300

    Update config.js

--- config.js
+++ config.js
@@ -3,14 +3,14 @@
 module.exports = {
   TOKEN: "",
   language: "en",
-  ownerID: ["1004206704994566164", ""], 
+  ownerID: ["921743618316836875", ""], 
   mongodbUri : "mongodb+srv://muhammed:muhsin121@cluster0.aod2oa4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
   setupFilePath: './commands/setup.json',
   commandsDir: './commands',  
   embedColor: "#1db954",
   activityName: "YouTube Music", 
   activityType: "LISTENING",  // Available activity types : LISTENING , PLAYING
-  SupportServer: "https://discord.gg/xQF9f9yUEM",
+  SupportServer: "https://discord.gg/QHzdUj2VqN",
   embedTimeout: 5, 
   errorLog: "", 
   nodes: [
