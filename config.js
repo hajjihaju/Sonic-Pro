8cef273f14892a0f84bb585ca62b2a112c0da072
hajjihaju <hajjihaju2@gmail.com>
2025-01-24 17:21:24 +0300

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ module.exports = {
   mongodbUri : "mongodb+srv://muhammed:muhsin121@cluster0.aod2oa4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
   setupFilePath: './commands/setup.json',
   commandsDir: './commands',  
-  embedColor: "#1db954",
+  embedColor: "#245c5c",
   activityName: "V.1.2", 
   activityType: "",  // Available activity types : LISTENING , PLAYING
   SupportServer: "https://discord.gg/QHzdUj2VqN",
