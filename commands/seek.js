d6bf7d010215cdd41b2ece12483a50990fd34840
hajjihaju <hajjihaju2@gmail.com>
2025-01-28 13:39:19 +0300

    Update seek.js

--- commands/seek.js
+++ commands/seek.js
@@ -1,16 +1,19 @@
 const { Message } = require('discord.js');
+const { getLanguage } = require('../utils/language'); // Adjust import path if necessary
 
 module.exports = {
     name: 'seek',
     description: 'Seeks to a specified time in the currently playing track',
     async execute(message, args) {
         const player = message.client.music.players.get(message.guild.id);
-        if (!player) return message.channel.send('No music is currently playing.');
+        const language = getLanguage(message); // Function to get the user's language
+        const langConfig = require(`../languages/${language}.json`);
 
-        if (!args[0]) return message.channel.send('Please provide the time in seconds to seek to.');
+        if (!player) return message.channel.send(langConfig.seek.noMusic);
+        if (!args[0]) return message.channel.send(langConfig.seek.noTime);
 
         const seekTime = parseInt(args[0]);
-        if (isNaN(seekTime)) return message.channel.send('Please provide a valid number.');
+        if (isNaN(seekTime)) return message.channel.send(langConfig.seek.invalidNumber);
 
         player.seek(seekTime * 1000); // Convert seconds to milliseconds
         message.channel.send(`Seeked to ${seekTime} seconds.`);
