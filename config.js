10ffec887862e3a053a80b53e2b06ddcd266b352
hajjihaju <hajjihaju2@gmail.com>
2025-03-07 23:45:47 +0300

    Update config.js

--- config.js
+++ config.js
@@ -29,6 +29,12 @@ module.exports = {
       host: "180.188.226.76",
       port:  7019,
       secure: false
-    }
+    },
+   {
+      name: "SPeed",
+      password: "saher.inzeworld.com",
+      host: "lava.inzeworld.com",
+      port:  3128,
+      secure: false
   ]
 }
